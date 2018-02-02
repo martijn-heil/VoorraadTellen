559b9d8a869baf87880aeb6fc8cc081e952db0b0
Martijn Heil <m.heil375@gmail.com>
2018-02-02 15:06:30 +0100

    Fix newline

--- src/main.c
+++ src/main.c
@@ -298,7 +298,7 @@ static bool save(struct csv_parser *parser, struct record *records, size_t recor
         if(csv_fwrite(f, header.columns[i], strlen(header.columns[i])) == EOF) return false;
         if(i != header.column_count - 1) fputc(delim, f);
     }
-    //fputc('\n', f); for some reason this doesn't have to be done.
+    fputc('\n', f);
 
     for(size_t i = 0; i < records_size; i++)
     {
